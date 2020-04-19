1b38aeec2414e0000533abbfbf09c3d54ed650ca
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2020-04-19 17:50:52 +0200

    :sparkles: introduce about me card

---
+++ src/components/aboutMeCard.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Paper } from '@material-ui/core'
+import aboutMeCardStyles from './aboutMeCard.module.css'
+import profilePic from '../../static/taiyr.png'
+import cvFile from "../../static/Taiyr_Begeyev_CV_EU.pdf"
+import { about_me } from "../config/text"
+
+const AboutMeCard = () => {
+  return (
+    <Paper className={aboutMeCardStyles.aboutMeWrapper}>
+      <div className={aboutMeCardStyles.aboutMePictureSection}>
+        <div className={aboutMeCardStyles.aboutMePictureWrapper}>
+          <img src={profilePic} className={aboutMeCardStyles.aboutMePicture} />
+        </div>
+        <div className={aboutMeCardStyles.aboutMeImage} />
+        <div className={aboutMeCardStyles.additionalSection}>
+          <a href={cvFile} target="_blank" className={aboutMeCardStyles.button}>Download CV</a>
+        </div>
+      </div>
+      <div className={aboutMeCardStyles.aboutMeInfoSection}>
+        <div className={aboutMeCardStyles.aboutMeInfo}>
+          <div className={aboutMeCardStyles.aboutMeInfoName}>
+            <h2 style={{margin: '0', padding: '0'}}>Taiyr Begeyev</h2>
+            <a href="mailto:taiyrbegeyev@gmail.com" className={aboutMeCardStyles.aboutMeInfoNameSub}>taiyrbegeyev@gmail.com</a>
+          </div>
+          <div className={aboutMeCardStyles.aboutMeInfoDescription}>
+            <p>{about_me}</p>
+          </div>
+        </div>
+      </div>
+    </Paper>
+  )
+}
+
+export default AboutMeCard
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,14 +4,19 @@ import { Flex } from "@theme-ui/components"
 import Layout from "../@lekoarts/gatsby-theme-minimal-blog/components/layout"
 import SEO from "../@lekoarts/gatsby-theme-minimal-blog/components/seo"
 import AboutMeIntro from "../components/aboutMeIntro"
+import AboutMeCard from "../components/aboutMeCard"
 import TimeLine from "../components/timeline"
+import aboutStyles from "./about.module.css"
 
 const About = () => {
   return (
     <Layout>
       <SEO title="About" />
-      <Flex sx={{ flexFlow: `wrap`, flexDirection: 'column' }}>
+      <Flex sx={{ flexFlow: `wrap`, flexDirection: 'column', alignItems: 'center' }}>
         <AboutMeIntro />
+        <div className={aboutStyles.page} id="aboutMeCard">
+          <AboutMeCard />
+        </div>
         <TimeLine />
       </Flex>
     </Layout>
