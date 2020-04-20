cb1268d9bd83bfa0a539b501c7a8b2357c488ac5
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2020-04-20 16:29:55 +0200

    :sparkles: add contact into footer

--- src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
@@ -2,6 +2,25 @@
 import { jsx, Styled } from "theme-ui"
 import useSiteMetadata from "../hooks/use-site-metadata"
 
+const links = [
+  {
+    id: 'github',
+    href: 'https://github.com/taiyrbegeyev',
+  },
+  {
+    id: 'linkedin',
+    href: 'https://www.linkedin.com/in/taiyrbegeyev',
+  },
+  {
+    id: 'twitter',
+    href: 'https://twitter.com/tbegeyev',
+  },
+  {
+    id: 'instagram',
+    href: 'https://www.instagram.com/taiyrbegeyev'
+  },
+]
+
 const Footer = () => {
   const { siteTitle } = useSiteMetadata()
 
@@ -16,12 +35,33 @@ const Footer = () => {
         a: {
           variant: `links.secondary`,
         },
-        flexDirection: [`column`, `column`, `row`],
+        flexDirection: [`column`, `row`, `row`],
         variant: `dividers.top`,
       }}
     >
       <div>
         &copy; {new Date().getFullYear()} by {siteTitle}. All rights reserved.
+      </div>
+      <div
+        sx={{
+          display: `flex`,
+          mt: [20, 0, 0],
+        }}
+      >
+        {links.map(item => (
+          <div key={item.id}>
+            <a href={item.href} target="_blank">
+              <img
+                sx={{
+                  margin: '0 0.5rem',
+                  filter: `invert(0.6)`,
+                }}
+                width="16"
+                src={`https://simpleicons.org/icons/${item.id}.svg`}
+              />
+            </a>
+          </div>
+        ))}
       </div>
     </footer>
   )
